ec2bf826d9f71408fd2037cff0067d34013f68e3
marcocareddu <saldumaccu@tuta.io>
2024-01-15 10:59:41 +0100

    First commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import List from "./List";
+
+function App() {
+
+  return (
+    <div className="container">
+      <h1>Prossimi Incontri</h1>
+      <div className="people-list">
+        <List />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/List.js
@@ -0,0 +1,16 @@
+import React from "react";
+import data from "./data";
+
+const List = () => {
+  return (
+  <ul>
+    <li>Caccap</li>
+  </ul>
+  );
+};
+
+const Person = () => {
+  return <article>persona</article>;
+};
+
+export default List;
